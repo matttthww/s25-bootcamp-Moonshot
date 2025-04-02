9ba8a5c36bb39f245f7a348592e64d3ac526d91e
matttthww <matthewtree1212@icloud.com>
2025-04-01 20:05:41 -0400

    Completed moonshot tutorial

---
+++ Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Matthew Castaneda on 3/31/25.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable{
+    let id:String
+    let name:String
+    let description:String
+}
---
+++ MoonshotTests/MoonshotTests.swift
@@ -0,0 +1,16 @@
+//
+//  MoonshotTests.swift
+//  MoonshotTests
+//
+//  Created by Matthew Castaneda on 3/31/25.
+//
+
+import Testing
+
+struct MoonshotTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
